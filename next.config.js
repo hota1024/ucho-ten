78a1e89471681def128172052eaded8182783716
= <hotalog@hota1024.com>
2023-04-15 01:56:33 +0900

    🔨enable appr dir

--- next.config.js
+++ next.config.js
@@ -3,8 +3,11 @@ const path = require('path')
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  experimental: {
+    appDir: true
+  },
   webpack(config, options) {
-    config.resolve.alias['@'] = path.resolve(__dirname, 'src')
+    config.resolve.alias['@'] = path.resolve(__dirname, 'app')
 
     return config
   }
