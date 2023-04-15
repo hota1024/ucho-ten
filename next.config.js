3b2ef8488e11cb70145c9020ce6345f1f7de6535
= <hotalog@hota1024.com>
2023-04-16 03:11:33 +0900

    🔨disable reactStrictMode

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 /** @type {import('next').NextConfig} */
 module.exports = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   experimental: {
     appDir: true
   },
