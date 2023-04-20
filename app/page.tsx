975e87cc34bffe89a203203bf40f35136239a836
ば <masaya.kurata@std.code.ac.jp>
2023-04-21 01:23:01 +0900

    ヘッダーが追加されたことによってページ全体が多少スクロール可能になるキモい現象を修正

--- app/page.tsx
+++ app/page.tsx
@@ -43,7 +43,7 @@ const HomePage: NextPage = () => {
 
   return (
     <MainLayout>
-      <div>
+      <div style={{height: '100vh', overflow: 'hidden' }}>
         <div
           style={{
             height: '5vh',
