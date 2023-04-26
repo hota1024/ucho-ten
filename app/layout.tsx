d7f877fc571b9f398d827b80b5a2bf5b7da0b280
= <hotalog@hota1024.com>
2023-04-26 16:33:52 +0900

    💄add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,16 @@ export default function RootLayout({
     <html lang="ja">
       <head>
         <title>Home | Ucho-ten</title>
+        <link rel="shortcut icon" href="favicon.svg" type="image/svg+xml" />
       </head>
-      <body style={{backgroundImage:'url(/images/backgroundimg/sky_00421.jpg)', backgroundSize: 'cover',backgroundColor:"rgba(255,255,255,0.1)",backgroundBlendMode:"lighten"}}>
+      <body
+        style={{
+          backgroundImage: 'url(/images/backgroundimg/sky_00421.jpg)',
+          backgroundSize: 'cover',
+          backgroundColor: 'rgba(255,255,255,0.1)',
+          backgroundBlendMode: 'lighten',
+        }}
+      >
         <Providers>{children}</Providers>
       </body>
     </html>
