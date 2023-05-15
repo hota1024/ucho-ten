afeb79b808ddfc68753288575309480a3d89ee15
ば <masaya.kurata@std.code.ac.jp>
2023-05-15 23:40:07 +0900

    空白文字postを対策

--- app/components/PostModal/index.tsx
+++ app/components/PostModal/index.tsx
@@ -59,8 +59,8 @@ export const PostModal = (props: PostModalProps) => {
   const textareaRef = useRef<HTMLTextAreaElement>(null)
   const [compressProcessing, setCompressProcessing] = useState(false)
 
-
-  const isPostable = contentText.length > 0
+  const reg = /^[\u0009-\u000d\u001c-\u0020\u11a3-\u11a7\u1680\u180e\u2000-\u200f\u202f\u205f\u2060\u3000\u3164\ufeff\u034f\u2028\u2029\u202a-\u202e\u2061-\u2063\ufeff]*$/;
+  const isPostable = contentText.length > 0 && !reg.test(contentText)
   const isContentLimitOver = contentText.length > 300
   const isImageMaxLimited =
     contentImage.length >= 5 || contentImage.length === 4 // 4枚まで
