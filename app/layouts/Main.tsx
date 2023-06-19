7ee80c838fc0211c86da8546b490db5b2ceb6b65
ば <masaya.kurata@std.code.ac.jp>
2023-06-20 00:15:08 +0900

    likeとrpの通知のみだった場合バッジを非表示に

--- app/layouts/Main.tsx
+++ app/layouts/Main.tsx
@@ -129,8 +129,17 @@ export const MainLayout: React.FC<MainLayoutProps> = (props) => {
 
     const id = setInterval(async () => {
       const result = await agent.countUnreadNotifications()
+      const notifications = await agent.listNotifications()
+      console.log(notifications)
+      let notify_num = 0
+      for(let i = 0; i < result.data.count; i++){
+        console.log(notifications.data.notifications[i])
+        if(notifications.data.notifications[i].reason === "follow" || notifications.data.notifications[i].reason === "mention" || notifications.data.notifications[i].reason === "reply" || notifications.data.notifications[i].reason === "quote"){
+          notify_num++
+        }
+      }
 
-      setNotificationCount(result.data.count)
+      setNotificationCount(notify_num)
     }, 10000)
 
     return () => {
