2c7076cc25937c3c0cc37594bb5f2f0377eb6435
ば <masaya.kurata@std.code.ac.jp>
2023-07-08 22:24:39 +0900

    作業内容push

--- app/components/PostThreadModal/index.tsx
+++ app/components/PostThreadModal/index.tsx
@@ -41,28 +41,58 @@ export type SetttingsModalProps = {
 export const SetttingsModal = (props: SetttingsModalProps) => {
     const { open, onClose, threadId } = props;
     const [agent] = useAgent();
-    const [threadData, setThreadData] = useState<FeedViewPost | undefined>(undefined);
+    const [threadData, setThreadData] = useState<FeedViewPost | undefined>(undefined)
+    const [nestedReplies, setNestedReplies] = useState<any[]>([])
+    const [isProssecing, setIsProcessing] = useState(false)
 
     useEffect(() => {
+        if (!open) {
+            return
+        }
+
         const getPostThread = async () => {
+            // ...
             if (!agent) {
                 return;
             }
 
             try {
+                setIsProcessing(true)
                 const result = await agent.app.bsky.feed.getPostThread({
                     uri: 'at://did:plc:owybkbkx7ph76awaqlldeccc/app.bsky.feed.post/3jztkpobp222z',
                 });
                 const postThread = result.data.thread;
-                setThreadData(postThread)
+                setThreadData(postThread);
+                console.log(postThread);
+
+                const nestedReplies = printNestedReplies(postThread.replies);
+                console.log(nestedReplies);
             } catch (error) {
                 console.error('Error retrieving post thread:', error);
             }
         };
 
-        if (open) {
-            getPostThread();
+        function printNestedReplies(posts: any[], result: any[] = []): any[] {
+            if(posts !== undefined){
+                for (const post of posts) {
+                    result.push(post.post);
+
+                    if (post.replies && post.replies.length > 0) {
+                        printNestedReplies(post.replies, result);
+                    }
+                }
+            }
+            return result
         }
+
+        if (open) {
+            getPostThread()
+            const result = printNestedReplies(threadData?.replies)
+            setNestedReplies(result)
+            console.log(result)
+            setIsProcessing(false)
+        }
+
     }, [agent, open]);
 
     if (!open) {
@@ -78,7 +108,15 @@ export const SetttingsModal = (props: SetttingsModalProps) => {
             </Modal.Header>
             <Modal.Body>
                 <Text>Threads are a way to organize your posts.</Text>
-                {threadData && <Text>{JSON.stringify(threadData)}</Text>}
+                {false ? ( // Check loading state before displaying the data
+                    <Text>Loading...</Text>
+                ) : (nestedReplies.map((post) => {
+                    console.log(post)
+                    return(
+                        <div>{post.record.text}</div>
+                    )
+                }))
+                }
             </Modal.Body>
             <Modal.Footer>
                 <Button onPress={onClose} flat css={{ width: '100%' }}>
