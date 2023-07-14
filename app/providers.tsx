1ab355c0b0c47b9a3a39e3dd3682707221f30985
ば <masaya.kurata@std.code.ac.jp>
2023-07-15 00:39:54 +0900

    Update providers.tsx

--- app/providers.tsx
+++ app/providers.tsx
@@ -17,39 +17,16 @@ import useDarkMode from 'use-dark-mode';
 
 const lightTheme = createTheme({
   type: 'light',
-  theme: {
-  }
 })
 
 const darkTheme = createTheme({
   type: 'dark',
-  theme: {
-  }
 })
 export function Providers({ children }: { children: React.ReactNode }) {
   const { isBrowser } = useSSR()
   const [isDark, setIsDark] = useState(false);
   const [mounted, setMounted] = useState(false)
   const darkMode = useDarkMode(false);
-
-  useEffect(() => {
-    // you can use any storage
-    let theme = window.localStorage.getItem('data-theme');
-    setIsDark(theme === 'dark');
-
-    const observer = new MutationObserver((mutation) => {
-      let newTheme = getDocumentTheme(document?.documentElement);
-      setIsDark(newTheme === 'dark');
-    });
-
-    // Observe the document theme changes
-    observer.observe(document?.documentElement, {
-      attributes: true,
-      attributeFilter: ['data-theme', 'style']
-    });
-
-    return () => observer.disconnect();
-  }, []);
 
   useServerInsertedHTML(() => {
     return <>{CssBaseline.flush()}</>
