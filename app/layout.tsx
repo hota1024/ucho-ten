d5afc8b429bf4c4ce942561cc8fc2eb62a80284e
ば <masaya.kurata@std.code.ac.jp>
2023-07-15 19:37:41 +0900

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,19 +9,16 @@ export default function RootLayout({
   return (
     <html lang="ja">
       <head>
-        <title>Home | Ucho-ten</title>
-        <link rel="shortcut icon" href="/favicon.svg" type="image/svg+xml" />
-        <meta name="robots" content="noarchive,max-image-preview"></meta>
-        <meta property="og:type" content="Ucho-tenは「他者から解放され、自己の独立」を目指すクライアントです。いつでも新鮮な気持ちでBlueskyを使うことができます。"></meta>
-        <meta property="og:url" content="https://ucho-ten.net"></meta>
-        <meta property="og:type" content="website"></meta>
-        <meta property="og:title" content="Ucho-ten Bluesky Client"></meta>
-        <meta property="og:description" content='Ucho-tenは「他者から解放され、自己の独立」を目指すクライアントです。いつでも新鮮な気持ちでBlueskyを使うことができます。'></meta>
-        <meta property="og:site_name" content="Ucho-ten"></meta>
-        <meta
-          property="og:image"
-          content="https://ucho-ten.net/images/Logo/ucho-ten-ogp.png"
-        ></meta>
+          <title>Home | Ucho-ten</title>
+          <link rel="shortcut icon" href="/favicon.svg" type="image/svg+xml" />
+          <meta name="robots" content="noarchive,max-image-preview"></meta>
+          <meta name="description" content="Ucho-tenは「他者から解放され、自己の独立」を目指すクライアントです。いつでも新鮮な気持ちでBlueskyを使うことができます。"></meta>
+          <meta property="og:type" content="website"></meta>
+          <meta property="og:url" content="https://ucho-ten.net"></meta>
+          <meta property="og:title" content="Ucho-ten Bluesky Client"></meta>
+          <meta property="og:description" content='Ucho-tenは「他者から解放され、自己の独立」を目指すクライアントです。いつでも新鮮な気持ちでBlueskyを使うことができます。'></meta>
+          <meta property="og:site_name" content="Ucho-ten"></meta>
+          <meta property="og:image" content="https://ucho-ten.net/images/Logo/ucho-ten-ogp.png"></meta>
           <meta name="twitter:card" content="summarylargeimage"></meta>
       </head>
       <body
