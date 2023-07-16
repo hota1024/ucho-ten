05b521afa8aed98fc922cbdb2a94aee492e9f962
ば <masaya.kurata@std.code.ac.jp>
2023-07-16 22:26:59 +0900

    Update providers.tsx

--- app/providers.tsx
+++ app/providers.tsx
@@ -20,32 +20,7 @@ const lightTheme = createTheme({
 })
 
 const darkTheme = createTheme({
-  type: "dark", // it could be "light" or "dark"
-  theme: {
-    colors: {
-      // brand colors
-      primaryLight: '$green200',
-      primaryLightHover: '$green300',
-      primaryLightActive: '$green400',
-      primaryLightContrast: '$green600',
-      primary: '#4ADE7B',
-      primaryBorder: '$green500',
-      primaryBorderHover: '$green600',
-      primarySolidHover: '$green700',
-      primarySolidContrast: '$white',
-      primaryShadow: '$green500',
-
-      gradient: 'linear-gradient(112deg, $blue100 -25%, $pink500 -10%, $purple500 80%)',
-      link: '#5E1DAD',
-
-      // you can also create your own color
-      myColor: '#ff4ecd'
-
-      // ...  more colors
-    },
-    space: {},
-    fonts: {}
-  }
+  type: "dark",
 })
 export function Providers({ children }: { children: React.ReactNode }) {
   const { isBrowser } = useSSR()
