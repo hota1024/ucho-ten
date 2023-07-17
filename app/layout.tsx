d1c365c89fe513c893ed75eea14467931e218a29
ば <masaya.kurata@std.code.ac.jp>
2023-07-17 12:22:55 +0900

    update head

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,19 +17,18 @@ export default function RootLayout({
         <html lang="ja">
         <head>
             <title>Home | Ucho-ten</title>
+            <meta charSet="utf-8"/>
+            <meta property="og:type" content="website"/>
+            <meta property="og:site_name" content="Ucho-ten"/>
+            <meta property="og:title" content="Ucho-ten Bluesky Client"/>
+            <meta property="og:url" content="https://ucho-ten.net"/>
+            <meta property="og:locale" content="ja_JP"  />
+            <meta name="description" content="Ucho-tenは「他者から解放され、自己の独立」を目指すBlueskyクライアントです。いつでも新鮮な気持ちでSNSを使うことができます。"/>
+            <meta property="og:description" content='Ucho-tenは「他者から解放され、自己の独立」を目指すクライアントです。いつでも新鮮な気持ちでBlueskyを使うことができます。'/>
+            <meta property="og:image" content="https://ucho-ten.net/images/Logo/ucho-ten-ogp.png"/>
+            <meta name="twitter:card" content="summarylargeimage"/>
             <link rel="shortcut icon" href="/favicon.svg" type="image/svg+xml" />
             <meta name="robots" content="noarchive,max-image-preview"></meta>
-            <meta property="og:type" content="Ucho-tenは「他者から解放され、自己の独立」を目指すクライアントです。いつでも新鮮な気持ちでBlueskyを使うことができます。"></meta>
-            <meta property="og:url" content="https://ucho-ten.net"></meta>
-            <meta property="og:type" content="website"></meta>
-            <meta property="og:title" content="Ucho-ten Bluesky Client"></meta>
-            <meta property="og:description" content='Ucho-tenは「他者から解放され、自己の独立」を目指すクライアントです。いつでも新鮮な気持ちでBlueskyを使うことができます。'></meta>
-            <meta property="og:site_name" content="Ucho-ten"></meta>
-            <meta
-                property="og:image"
-                content="https://ucho-ten.net/images/Logo/ucho-ten-ogp.png"
-            ></meta>
-            <meta name="twitter:card" content="summarylargeimage"></meta>
         </head>
         <ComponentWithNoSSR>
             <Providers>
