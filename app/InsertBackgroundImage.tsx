89760fd39c827e768d04edfeffe8f312525c4775
ば <masaya.kurata@std.code.ac.jp>
2023-07-17 10:20:05 +0900

    Update InsertBackgroundImage.tsx

--- app/InsertBackgroundImage.tsx
+++ app/InsertBackgroundImage.tsx
@@ -45,8 +45,8 @@ export const InsertBackgroundImage: React.FC<InsertBackgroundImageProps> = ({ ch
             style={{
                 backgroundImage: appearanceColorMode === 'dark' ? `url(${darkModeBGI})` : appearanceColorMode === "light" ? `url(${lightModeBGI})` : darkMode.value ? `url(${darkModeBGI})` : `url(${lightModeBGI})`,
                 backgroundSize: 'cover',
-                backgroundColor: ( appearanceColorMode === 'dark' || appearanceColorMode !== "light" && darkMode.value) ? 'rgba(0,0,0,0.2)' : 'rgba(255,255,255,0.1)',
-                backgroundBlendMode: ( appearanceColorMode === 'dark' || appearanceColorMode !== "light" && darkMode.value) ? 'darken' : 'lighten',
+                backgroundColor: ( appearanceColorMode === 'dark' || appearanceColorMode === "system" && darkMode.value) ? 'rgba(0,0,0,0.2)' : 'rgba(255,255,255,0.1)',
+                backgroundBlendMode: ( appearanceColorMode === 'dark' || appearanceColorMode === "system" && darkMode.value) ? 'darken' : 'lighten',
             }}
         >
         {children}
