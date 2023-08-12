87ba4d81d2bbceab8f15cc26de38ebbebd7f2325
ば <masaya.kurata@std.code.ac.jp>
2023-08-12 20:08:08 +0900

    できたけど型エラーでる

--- packages/bluesky-react/src/hooks/usePostLazy/index.ts
+++ packages/bluesky-react/src/hooks/usePostLazy/index.ts
@@ -1,3 +1,4 @@
+import type { AppBskyFeedGetPostThread } from "@atproto/api";
 import type { PostView } from "@atproto/api/dist/client/types/app/bsky/feed/defs";
 import { useCallback, useEffect, useState } from "react";
 
@@ -6,7 +7,18 @@ import { usePostStore } from "@/states";
 import { useClient } from "../useClient";
 import type { UsePostLazyReturn } from "./type";
 
-export function usePostLazy(): UsePostLazyReturn {
+/**
+ * returns user profile that specified in `params.actor`.
+ *
+ * @param params `AppBskyFeedGetPostThread.QueryParams`
+ * @param opts `UseProfileOpts`
+ * @returns `UseProfileReturn`
+ */
+
+export function usePostLazy(
+    params?: AppBskyFeedGetPostThread.QueryParams,
+    opts?: AppBskyFeedGetPostThread.CallOptions,
+): UsePostLazyReturn {
   // shared states //
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const client = useClient();
@@ -20,12 +32,39 @@ export function usePostLazy(): UsePostLazyReturn {
   // functions //
   const fetchPost = useCallback(async (uri: string) => {
     // TODO: 実装する。
-    return {} as any;
+
+    try {
+      setError(null);
+      setLoading(true);
+
+      const res = await client.agent.getPostThread({uri: uri}, opts);
+      const {post} = res.data.thread
+      console.log(post)
+      // @ts-expect-error aaaaa
+      merge(new Map([[post.uri, post]]));
+      // @ts-expect-error aaaaa
+      setPost(post);
+    } catch (error) {
+      setError(error);
+    } finally {
+      setLoading(false);
+    }
+
+    return {post} as any;
   }, []);
 
   // effects //
   useEffect(() => {
     // TODO: 実装する。
+    if (!post) {
+      return;
+    }
+
+    const newPost = posts.get(post.uri);
+
+    if (newPost) {
+      setPost(newPost);
+    }
   }, [post, posts]);
 
   return { post, fetchPost, loading, error };
