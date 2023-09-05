e1ca8e19e2bd063f10e683b141d042ad104662ec
ば <masaya.kurata@std.code.ac.jp>
2023-09-06 05:06:13 +0900

    作業分push

--- apps/web/src/components/pages/CreatePostPage/CreatePostPage.tsx
+++ apps/web/src/components/pages/CreatePostPage/CreatePostPage.tsx
@@ -31,8 +31,16 @@ export function CreatePostPage() {
     const [compressProcessing, setCompressProcessing] = useState(false)
     const { Container,
             Header, HeaderPostButton, HeaderTitle,
-            Content,
-            Footer, FooterBackgroundColor } = createPostPage();
+            Content, ContentTextArea,
+            Footer, FooterBackgroundColor,
+            PostModal,
+            header, headerTitle, headerPostButton,
+            content, contentLeft, contentLeftAuthorIcon,
+                     contentRight, contentRightTextArea, contentRightImagesContainer,
+            footer, footerTooltip,
+                    footerCharacterCount, footerCharacterCountText, footerCharacterCountCircle,
+                    footerTooltipStyle,
+    } = createPostPage();
 
     const handlePostClick = async () => {
         setLoading(true)
@@ -70,151 +78,109 @@ export function CreatePostPage() {
 
   return (
       <main className={" w-full h-full"} style={{backgroundColor:'rgba(0, 0, 0, 0.33)', backgroundImage:'url(https://raw.githubusercontent.com/kawaikute-gomen/ucho-ten-images-repo/main/images/backgroundImages/light/sky_00421.jpg)'}}>
-          <div className={Container()} style={{backgroundColor:'rgba(218, 218, 218, 0.68)'}}>
-              <div className={Footer()}>
-                  <div className={FooterBackgroundColor()} />
-                  <div className="Plusbuton left-[168px] top-[397px] absolute text-white text-xl font-black">
-                      <FontAwesomeIcon icon={faCirclePlus}/>
-                  </div>
-                  <div className="AddImageButton left-[36px] top-[395px] absolute text-white text-2xl font-normal">
-                      <label htmlFor={inputId}>
-                          <Button
-                              disabled={loading || compressProcessing || isImageMaxLimited}
-                              as="span"
-                              startContent={<FontAwesomeIcon icon={faImage} />}
-                          />
-
-                          <input
-                              hidden
-                              id={inputId}
-                              type="file"
-                              multiple
-                              accept="image/*,.png,.jpg,.jpeg,.webp,.gif,.svg,.bmp,.tiff,.avif,.heic,.heif"
-                              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                                  handleOnAddImage(e)
-                              }
-                              disabled={loading || compressProcessing || isImageMaxLimited }
-                          />
-                      </label>
-                  </div>
-                  <div className="Lang left-[96px] top-[400px] absolute text-white text-base font-bold">
-                      <Dropdown backdrop="blur">
-                          <DropdownTrigger>
-                              <Button
-                                variant="bordered"
-                              >
-                                  {PostContentLanguage.size === 1 ? PostContentLanguage : "Langs"}
-                              </Button>
-                          </DropdownTrigger>
-                          <DropdownMenu
-                              aria-label="Multiple selection actions"
-                              selectionMode="multiple"
-                              disallowEmptySelection
-                              selectedKeys={PostContentLanguage}
-                              //@ts-ignore
-                              onSelectionChange={PostContentLanguage.size >= 3 ? null : setPostContentLanguage}
-                          >
-                              <DropdownItem key='es'>Espalier</DropdownItem>
-                              <DropdownItem key='fr'>Francais</DropdownItem>
-                              <DropdownItem key='de'>Deutsch</DropdownItem>
-                              <DropdownItem key='it'>Italiano</DropdownItem>
-                              <DropdownItem key='pt'>Portuguese</DropdownItem>
-                              <DropdownItem key='ru'>Русский</DropdownItem>
-                              <DropdownItem key='zh'>中文</DropdownItem>
-                              <DropdownItem key='ko'>한국어</DropdownItem>
-                              <DropdownItem key='en'>English</DropdownItem>
-                              <DropdownItem key='ja'>日本語</DropdownItem>
-                          </DropdownMenu>
-                      </Dropdown>
-                  </div>
-                  <div className=" w-14 h-5 left-[315px] top-[398px] absolute">
-                      <div className=" left-0 top-[4px] absolute text-white text-sm">
-                          {300 - contentText.length}
-                      </div>
-                      <div className="Ellipse1 w-5 h-5 left-[34px] top-[1px] absolute bg-zinc-300 rounded-full" >
-                          <CircularProgressbar maxValue={300} value={contentText.length} background={true}/>
-                      </div>
-                  </div>
+          <div className={PostModal()}>
+              <div className={header()}>
+                  <div className={headerTitle()}>Post</div>
+                  <button className={headerPostButton()}>send</button>
               </div>
-              <div className={Content()}>
-                  {contentImage.length > 0 && (
-                      <div className="Footer w-96 h-28 left-0 top-[233px] absolute">
-                          <div className=" w-96 h-20 left-[13px] top-[10px] absolute">
+              <div className={content()}>
+                  <div className={contentLeft()}>
+                      <div className={contentLeftAuthorIcon()}>
+                          <div style={{width:'100%', height:'100%', overflow:'hidden'}}></div>
+                      </div>
+                  </div>
+                  <div className={contentRight()}>
+                      <div className={`w-full ${contentImage.length > 0 ? "h-[calc(100%-105px)]" : "h-full"}`}>
+                          <textarea className={contentRightTextArea()}
+                                    style={{backgroundColor:'transparent', resize:'none', outline: 'none'}}
+                                    placeholder={"Yo, Do you do Brusco?"}
+                                    autoFocus={true}
+                                    onChange={(e) => {
+                                        setContentText(e.target.value)
+                                    }}/>
+                      </div>
+                      {contentImage.length > 0 && (
+                          <div className={contentRightImagesContainer()}>
                               {contentImage.map((image, index) => (
-                                  <div className={` w-20 h-20 left-[${index * 93}px] top-0 absolute bg-zinc-300 rounded-lg overflow-hidden `}>
+                                  <div key={index} className={"relative w-1/4 h-full"}>
                                       <Image
                                           src={URL.createObjectURL(image)}
-                                          alt="preview"
-                                          style={{
-                                              width: `100%`,
-                                              height: '85px',
-                                              objectFit: 'cover',
-                                          }}
-                                      ></Image>
-                                      <div style={{ zIndex:10, position: 'absolute', top: 0, right: 0 }}>
-                                          <Button
-                                              className={" w-[24px]"}
-                                              style={{
-                                                  backgroundColor: 'rgba(0,0,0,0.3)',
-                                              }}
-                                              onClick={() => handleOnRemoveImage(index)}
-                                              startContent={<FontAwesomeIcon className=" text-white" icon={faXmark} size="lg" />}
-                                              radius={"full"}
-                                          >
-                                          </Button>
-                                      </div>
-                                      <div style={{ zIndex:10, position: 'absolute', bottom: 0, right: 0 }}>
-                                          <Button
-                                              className={" w-[24px]"}
-                                              style={{
-                                                  backgroundColor: 'rgba(0,0,0,0.3)',
-                                              }}
-                                              onClick={() => handleOnRemoveImage(index)}
-                                              radius={"full"}
-                                          >ALT
-                                          </Button>
-                                      </div>
-                                      <div style={{ zIndex:10, position: 'absolute', bottom: 0, left: 0 }}>
-                                          <Button
-                                              style={{
-                                                  width:'24px',
-                                                  backgroundColor: 'rgba(0,0,0,0.3)',
-                                              }}
-                                              onClick={() => handleOnRemoveImage(index)}
-                                              startContent={<FontAwesomeIcon className=" text-white" icon={faPen} size="lg" />}
-                                              radius={"full"}
-                                          >
-                                          </Button>
+                                          alt="image"
+                                          style={{borderRadius:'10px', objectFit:'cover'}}
+                                          className={"h-[85px] w-[85px] object-cover object-center"}
+                                      />
+                                      <div className={"z-10 absolute top-0 left-0"}>
+                                          <button onClick={() => handleOnRemoveImage(index)}>
+                                              <FontAwesomeIcon icon={faXmark} size={"sm"}></FontAwesomeIcon>
+                                          </button>
                                       </div>
                                   </div>
                               ))}
                           </div>
-                      </div>
-                  )}
-                  <div className={`Textarea w-80 h-${contentImage.length > 0 ? 56 : "full"} left-[53px] top-0 absolute`}>
-                      <textarea className="Yo w-full h-full left-0 top-0 absolute text-black font-medium"
-                                style={{backgroundColor:'transparent', resize:'none', outline: 'none'}}
-                                placeholder={"Yo, Do you Brusco?"}
-                                autoFocus={true}
-                                onChange={(e) => {
-                                    setContentText(e.target.value)
-                                }}/>
-                  </div>
-                  <div className=" w-14 h-56 left-0 top-0 absolute">
-                      <div className=" w-7 h-7 left-[11px] top-0 absolute bg-zinc-300 rounded-lg" />
-                      <img className="Bafkreihwad5kaujw2f6kbfg37zmkhclgd3ap7grixl6pusfb5b34s6jite11 w-7 h-7 left-[11px] top-0 absolute rounded-lg" src="https://via.placeholder.com/30x30" />
+                      )}
                   </div>
               </div>
-              <div className={Header()}>
-                  <div className="PostButton w-24 h-9 right-[4px] top-[3px] absolute">
-                      <Button
-                          variant="shadow"
-                          className="PostButton w-24 h-9 left-0 top-0 absolute rounded-3xl text-white text-sm font-medium"
-                          style={{backgroundColor:'rgba(0, 130, 250, 1)'}}
-                          onClick={handlePostClick}
-                      >send</Button>
+              <div className={footer()}>
+                  <div className={footerTooltip()}>
+                      <div className={footerTooltipStyle()}>
+                          <label htmlFor={inputId}>
+                              <Button
+                                  disabled={loading || compressProcessing || isImageMaxLimited}
+                                  as="span"
+                                  startContent={<FontAwesomeIcon icon={faImage} style={{height:'100%'}}/>}
+                              />
+
+                              <input
+                                  hidden
+                                  id={inputId}
+                                  type="file"
+                                  multiple
+                                  accept="image/*,.png,.jpg,.jpeg,.webp,.gif,.svg,.bmp,.tiff,.avif,.heic,.heif"
+                                  onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                                      handleOnAddImage(e)
+                                  }
+                                  disabled={loading || compressProcessing || isImageMaxLimited }
+                              />
+                          </label>
+                      </div>
+                      <div className={footerTooltipStyle()} style={{bottom:'5%'}}>
+                          <Dropdown backdrop="blur">
+                              <DropdownTrigger>
+                                  <Button
+                                      variant="bordered"
+                                  >
+                                      {PostContentLanguage}
+                                  </Button>
+                              </DropdownTrigger>
+                              <DropdownMenu
+                                  aria-label="Multiple selection actions"
+                                  selectionMode="multiple"
+                                  disallowEmptySelection
+                                  selectedKeys={PostContentLanguage}
+                                  //@ts-ignore
+                                  onSelectionChange={PostContentLanguage.size >= 3 ? null : setPostContentLanguage}
+                              >
+                                  <DropdownItem key='es'>Espalier</DropdownItem>
+                                  <DropdownItem key='fr'>Francais</DropdownItem>
+                                  <DropdownItem key='de'>Deutsch</DropdownItem>
+                                  <DropdownItem key='it'>Italiano</DropdownItem>
+                                  <DropdownItem key='pt'>Portuguese</DropdownItem>
+                                  <DropdownItem key='ru'>Русский</DropdownItem>
+                                  <DropdownItem key='zh'>中文</DropdownItem>
+                                  <DropdownItem key='ko'>한국어</DropdownItem>
+                                  <DropdownItem key='en'>English</DropdownItem>
+                                  <DropdownItem key='ja'>日本語</DropdownItem>
+                              </DropdownMenu>
+                          </Dropdown>
+                      </div>
+                      <FontAwesomeIcon className={footerTooltipStyle()} icon={faCirclePlus} size={"sm"}></FontAwesomeIcon>
+                      <div className={footerCharacterCount()}>
+                          <div className={footerCharacterCountText()} style={{color:contentText.length >= 300 ? "red": 'white'}}>{300 - contentText.length}</div>
+                          <div style={{width:'20px', height:'20px', marginLeft:'5px'}}>
+                              <CircularProgressbar value={contentText.length} maxValue={300} />
+                          </div>
+                      </div>
                   </div>
-                  <div className={HeaderTitle()}>Post</div>
               </div>
           </div>
       </main>
--- apps/web/src/components/pages/CreatePostPage/styles.ts
+++ apps/web/src/components/pages/CreatePostPage/styles.ts
@@ -4,11 +4,30 @@ export const createPostPage = tv({
     slots: {
         Container: "w-96 h-96 relative",
         Header: "w-96 h-11 left-0 top-0 absolute",
-        HeaderTitle: "w-9 left-[177.46px] top-[11px] absolute text-white text-base font-bold",
+        HeaderTitle: "w-9 left-[177.46px] top-[11px] absolute text-white text-base font-bold ",
         HeaderPostButton: "w-24 h-9 right-[4px] top-[3px] absolute",
         Content: 'w-96 h-80 left-0 top-[43px] absolute',
+        ContentTextArea: "w-full h-full left-0 top-0 absolute text-black font-medium bg-transparent resize-none outline-none",
+        ContentImagesContainer: "Footer w-96 h-28 left-0 top-[233px] absolute",
         Footer: "w-96 h-96 left-0 top-0 absolute",
         FooterBackgroundColor: "w-96 h-14 left-0 top-[381px] absolute bg-zinc-300",
+        PostModal: 'fontFamily-[Noto Sans JP] w-full max-w-[500px] height-full max-h-[750px] position-[relative] bg-[#DADADA] bg-opacity-70 relative',
+        header: 'w-full h-[43px] top-0 relative select-none',
+            headerTitle: 'w-full h-full text-center text-white text-base font-medium fontSize-[16px] items-center flex justify-center',
+            headerPostButton: 'w-[91px] h-[37px] right-[4px] top-[3px] absolute',
 
+        content: 'w-full h-[400px] relative flex items-center flex-wrap',
+            contentLeft: 'w-[54px] h-full relative flex flex-col items-center',
+                contentLeftAuthorIcon: 'w-[30px] h-[30px] relative bg-black rounded-[10px]',
+            contentRight: 'w-[calc(100%-54px)] h-full relative',
+                contentRightTextArea: 'w-full h-full relative bg-transparent resize-none outline-none placeholder-[#808080]',
+                contentRightImagesContainer: 'w-full h-[105px]　flex items-center',
+
+        footer: 'w-full h-[43px] bottom-0 relative bg-[#DADADA] relative select-none',
+            footerTooltip: 'h-full w-full flex justify-left items-left text-white',
+            footerTooltipStyle:'h-[20px] relative ml-10　mt-3',
+            footerCharacterCount: 'h-full w-full flex justify-end items-center text-white mr-10',
+                footerCharacterCountText: 'text-[20px]',
+                footerCharacterCountCircle: 'w-[20px] h-[20px] mr-[10px]',
     }
 });
