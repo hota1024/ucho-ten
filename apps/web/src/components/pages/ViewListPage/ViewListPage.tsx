e134c6ded421026f88736b45bd9f8221ed8ffae9
ば <masaya.kurata@std.code.ac.jp>
2023-09-13 21:35:06 +0900

    ミュートワードの削除をスワイプで実装

--- apps/web/src/components/pages/ViewListPage/ViewListPage.tsx
+++ apps/web/src/components/pages/ViewListPage/ViewListPage.tsx
@@ -7,8 +7,14 @@ import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
 import { useDropzone, FileWithPath } from 'react-dropzone'
 import 'react-circular-progressbar/dist/styles.css';
-import data from '@emoji-mart/data'
-import Picker from '@emoji-mart/react'
+import {
+    LeadingActions,
+    SwipeableList,
+    SwipeableListItem,
+    SwipeAction,
+    TrailingActions,
+} from 'react-swipeable-list';
+import 'react-swipeable-list/dist/styles.css';
 import {
     Dropdown,
     DropdownTrigger,
@@ -24,7 +30,7 @@ import {
 
 import {Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, useDisclosure} from "@nextui-org/react";
 
-import Textarea from 'react-textarea-autosize'; // 追加
+import {Accordion, AccordionItem} from "@nextui-org/react";
 
 interface Props {
     className?: string
@@ -37,35 +43,64 @@ interface Props {
 export const CreatePostPage: React.FC<Props> = (props: Props) => {
     const {className, color, isMobile, uploadImageAvailable, open} = props;
     const reg = /^[\u0009-\u000d\u001c-\u0020\u11a3-\u11a7\u1680\u180e\u2000-\u200f\u202f\u205f\u2060\u3000\u3164\ufeff\u034f\u2028\u2029\u202a-\u202e\u2061-\u2063\ufeff]*$/;
-    const [PostContentLanguage, setPostContentLanguage] = useState(new Set([navigator.language]))
-    const inputId = Math.random().toString(32).substring(2)
-    const [contentText, setContentText] = useState("");
-    const [contentImage, setContentImages] = useState<File[]>([])
     const [loading, setLoading] = useState(false)
-    const textareaRef = useRef<HTMLTextAreaElement>(null)
-    const hiddenInput = useRef<HTMLDivElement>(null)
-    const [isDetectedURL, setIsDetectURL] = useState(false)
-    const [detectedURLs, setDetectURLs] = useState<string[]>([])
-    const [selectedURL, setSelectedURL] = useState<string>('')
-    const [isOGPGetProcessing, setIsOGPGetProcessing] = useState(false)
-    const [isSetURLCard, setIsSetURLCard] = useState(false)
-    const [getOGPData, setGetOGPData] = useState<any>(null)
-    const [isGetOGPFetchError, setIsGetOGPFetchError] = useState(false)
-    const isImageMaxLimited =
-        contentImage.length >= 5 || contentImage.length === 4 // 4枚まで
-    const isImageMinLimited = contentImage.length === 0 // 4枚まで
-    const [compressProcessing, setCompressProcessing] = useState(false)
-    const { background, SearchBar, SearchBarInputArea,
+    const { background, SearchBar, SearchBarInputArea,Label,SearchButton,
     } = createPostPage();
+    
+    const trailingActions = () => (
+        <TrailingActions>
+            <SwipeAction
+                destructive={true}
+                onClick={() => console.info('swipe action triggered')}
+            >
+                <div style={{backgroundColor:'red', color:'white',}}>delete</div>
+            </SwipeAction>
+        </TrailingActions>
+    );
 
   return (
       <main className={background({color:color, isMobile:isMobile})}>
           <div className={SearchBar()}>
               <input className={SearchBarInputArea()} placeholder={'search already set mute word or category'}/>
-              <Button isIconOnly variant={'light'} disableRipple className={'absolute right-[12px]'}>
+              <Button isIconOnly variant={'light'} disableRipple className={SearchButton()}>
                   <FontAwesomeIcon icon={faMagnifyingGlass}></FontAwesomeIcon>
               </Button>
           </div>
+          <div className={Label()}>Category</div>
+          <Accordion>
+              <AccordionItem key="1" aria-label="Accordion 1" title="下ネタ">
+                  <SwipeableList>
+                      <SwipeableListItem
+                          trailingActions={trailingActions()}
+                          maxSwipe={100} // You can adjust this value as needed
+                      >
+                          <div className={'h-[54px] w-full flex items-center border-b border-t '}>ちんちん</div>
+                      </SwipeableListItem>
+                  </SwipeableList>
+                  <SwipeableList>
+                      <SwipeableListItem
+                          trailingActions={trailingActions()}
+                          maxSwipe={100} // You can adjust this value as needed
+                      >
+                          <div className={'h-[54px] w-full flex items-center border-b border-t '}>うんこ</div>
+                      </SwipeableListItem>
+                  </SwipeableList>
+                  <SwipeableList>
+                      <SwipeableListItem
+                          trailingActions={trailingActions()}
+                          maxSwipe={100} // You can adjust this value as needed
+                      >
+                          <div className={'h-[54px] w-full flex items-center border-b border-t '}>おもろすぎ</div>
+                      </SwipeableListItem>
+                  </SwipeableList>
+              </AccordionItem>
+              <AccordionItem key="2" aria-label="Accordion 2" title="Accordion 2">
+                  2
+              </AccordionItem>
+              <AccordionItem key="3" aria-label="Accordion 3" title="Accordion 3">
+                  3
+              </AccordionItem>
+          </Accordion>
       </main>
   );
 }
--- apps/web/src/components/pages/ViewListPage/styles.ts
+++ apps/web/src/components/pages/ViewListPage/styles.ts
@@ -3,31 +3,25 @@ import { tv } from "@nextui-org/react";
 export const createPostPage = tv({
     slots: {
         background: 'w-full h-full max-w-[500px]',
-        backgroundColor: 'w-full h-full bg-[#000000] bg-opacity-10 absolute',
-        SearchBar: 'bg-white h-[54px] w-full flex items-center justify-center',
+        SearchBar: 'bg-white h-[54px] w-full flex items-center justify-center relative',
         SearchBarInputArea: 'h-[44px] w-full bg-[#E9E9E9] ml-[12px] mr-[12px] placeholder-[#7D7D7D] outline-none pl-[20px] pr-[40px]',
+        SearchButton: 'absolute right-[12px]',
+        Label: 'w-full h-[40px] border-t-[1px] border-b-[1px] pl-[26px] flex items-center',
     },
     variants: {
         color:{
             light: {
-                background: 'bg-cover bg-[url("https://raw.githubusercontent.com/kawaikute-gomen/ucho-ten-images-repo/main/images/backgroundImages/light/sky_00421.jpg")]',
-                PostModal: 'bg-[#DADADA] bg-opacity-70 text-white',
-                footer: 'bg-[#DADADA]',
+                background: 'text-black',
             },
             dark: {
-                background: 'bg-cover bg-[url("https://raw.githubusercontent.com/kawaikute-gomen/ucho-ten-images-repo/main/images/backgroundImages/dark/starry-sky-gf5ade6b4f_1920.jpg")]',
-                PostModal: 'bg-[#2C2C2C] bg-opacity-70 text-[#D7D7D7]',
-                footer: 'bg-[#2C2C2C]',
+                background: 'text-white ',
+                Label: 'border-[#181818]',
             },
         },
         isMobile: {
             true: {
-                PostModal: "rounded-none",
-                background: "",
             },
             false: {
-                PostModal: "rounded-[10px] overflow-hidden min-h-[400px]",
-                background: "relative flex justify-center items-center",
             },
         },
     }
