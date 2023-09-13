03bc662428f12803cd516d8ab20eae6693ee0abb
ば <masaya.kurata@std.code.ac.jp>
2023-09-13 16:31:40 +0900

    created FeedPage

--- apps/web/src/components/pages/ViewFeedPage/styles.ts
+++ apps/web/src/components/pages/ViewFeedPage/styles.ts
@@ -1,30 +1,33 @@
 import { tv } from "@nextui-org/react";
 
-export const viewProfilePage = tv({
+export const viewFeedPage = tv({
     slots: {
         background: 'max-w-[500px] min-w-[350px]',
         ProfileContainer: 'w-full h-full bg-white border-[#E3E3E3] border-bottom-[1px]',
         HeaderImageContainer: 'w-full h-[130px] relative',
         ProfileHeaderImage: 'h-full w-full object-cover',
-        ProfileInfoContainer: 'w-full h-full relative pl-[13px] pr-[8px] pb-[16px]',
-        ProfileImage: 'h-[80px] w-[80px] rounded-[10px] top-[-24px] absolute',
-        ProfileDisplayName: 'font-black text-[24px]',
-        ProfileHandle: '',
-        ProfileBio: 'mt-[8px] ml-[4px] mr-[20px]',
-        ProfileCopyButton: 'h-[32px] w-[32px] ml-[10px] mr-[10px] border-[2px] border-[#929292] rounded-full flex justify-center items-center cursor-pointer',
-        ProfileActionButton: 'h-[32px] w-[32px] ml-[10px] mr-[10px] border-[2px] border-[#929292] rounded-full flex justify-center items-center cursor-pointer',
+        ProfileInfoContainer: 'w-full h-full relative pl-[13px] pr-[8px] pb-[16px] pt-[21px]',
+        ProfileImage: 'h-[50px] w-[50px] rounded-[10px] absolute',
+        ProfileDisplayName: 'font-black text-[24px] mt-[12px] ml-[3px]',
+        ProfileHandle: 'text-[#B8B8B8] font-medium ml-[3px]',
+        ProfileBio: 'mt-[8px] mr-[20px] text-[#B8B8B8] font-medium ml-[3px]',
+        ProfileCopyButton: 'h-[32px] w-[32px] ml-[10px] mr-[10px] flex justify-center items-center cursor-pointer',
+        ProfileActionButton: 'h-[32px] w-[32px] ml-[10px] mr-[10px] flex justify-center items-center cursor-pointer',
         FollowButton: 'mr-[8px] ml-[10px]',
-        Buttons: 'flex justify-end h-[56px] w-full flex items-center',
-        PropertyButton: 'text-[#929292]',
+        Buttons: 'flex justify-end h-[50px] w-full flex items-center',
+        PinButton: '',
+        ShareButton: '',
         PostContainer: 'w-full h-full',
     },
     variants: {
         color:{
             light: {
                 background: 'bg-cover bg-[url("https://raw.githubusercontent.com/kawaikute-gomen/ucho-ten-images-repo/main/images/backgroundImages/light/sky_00421.jpg")]',
+                ShareButton: 'text-black'
             },
             dark: {
                 background: 'bg-cover bg-[url("https://raw.githubusercontent.com/kawaikute-gomen/ucho-ten-images-repo/main/images/backgroundImages/dark/starry-sky-gf5ade6b4f_1920.jpg")]',
+                ShareButton: 'text-white'
             },
         },
         isMobile: {
@@ -37,5 +40,13 @@ export const viewProfilePage = tv({
                 background: "relative justify-center",
             },
         },
+        isPinned: {
+            true: {
+                PinButton: 'text-[#016EFF]'
+            },
+            false: {
+                PinButton: 'text-[#929292]'
+            }
+        }
     }
 });
