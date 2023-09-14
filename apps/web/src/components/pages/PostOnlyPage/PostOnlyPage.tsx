3c718312f53119a9f623ad6d2331ba85a4e0e9ea
ば <masaya.kurata@std.code.ac.jp>
2023-09-15 02:10:02 +0900

    actionに対してswipe操作を導入

--- apps/web/src/components/pages/PostOnlyPage/PostOnlyPage.tsx
+++ apps/web/src/components/pages/PostOnlyPage/PostOnlyPage.tsx
@@ -3,10 +3,8 @@ import { postOnlyPage } from "./styles";
 import { BrowserView, MobileView, isMobile } from "react-device-detect"
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import { faComment, faSquare as faRegularSquare, faBookmark as faRegularBookmark, } from '@fortawesome/free-regular-svg-icons'
-import { faRetweet, faSquare as faSolidSquare, faBookmark as faSolidBookmark, faEllipsis, faArrowUpFromBracket, faTrash, faFlag, faCode, faUser, faU, faLanguage} from '@fortawesome/free-solid-svg-icons'
+import { faRetweet, faQuoteLeft, faSquare as faSolidSquare, faBookmark as faSolidBookmark, faEllipsis, faArrowUpFromBracket, faTrash, faFlag, faCode, faUser, faU, faLanguage} from '@fortawesome/free-solid-svg-icons'
 import 'react-circular-progressbar/dist/styles.css';
-import data from '@emoji-mart/data'
-import Picker from '@emoji-mart/react'
 import {
     Dropdown,
     DropdownTrigger,
@@ -19,6 +17,15 @@ import {
     Input,
     Popover, PopoverTrigger, PopoverContent,
 } from "@nextui-org/react";
+
+import {
+    LeadingActions,
+    SwipeableList,
+    SwipeableListItem,
+    SwipeAction,
+    TrailingActions,
+} from 'react-swipeable-list';
+import 'react-swipeable-list/dist/styles.css';
 
 import {Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, useDisclosure} from "@nextui-org/react";
 
@@ -50,8 +57,24 @@ export const PostOnlyPage: React.FC<Props> = (props: Props) => {
     const { background, Container,AuthorPost,Author, AuthorIcon, AuthorDisplayName,AuthorHandle,PostContent,PostCreatedAt,ReactionButtonContainer,ReactionButton,
     } = postOnlyPage();
 
-  // @ts-ignore
-    // @ts-ignore
+    const leadingActions = () => (
+        <LeadingActions>
+            <SwipeAction onClick={() => console.info('swipe action triggered')}>
+                <span style={{backgroundColor:'cyan'}}>Reply</span>
+            </SwipeAction>
+        </LeadingActions>
+    );
+
+    const trailingActions = () => (
+        <TrailingActions>
+            <SwipeAction
+                onClick={() => console.info('swipe action triggered')}
+            >
+                <span style={{backgroundColor:'pink'}}>Like</span>
+            </SwipeAction>
+        </TrailingActions>
+    );
+
     return (
       <main className={background({color:color, isMobile:isMobile})}>
           <div className={Container({color:color})}>
@@ -64,9 +87,15 @@ export const PostOnlyPage: React.FC<Props> = (props: Props) => {
                       </div>
                   </div>
                   <div className={PostContent()}>
-                      <div>
-                          親譲の無鉄砲で小供の時から損ばかりしている。小学校に居る時分学校の二階から飛び降りて一週間ほど腰こしを抜ぬかした事がある。なぜそんな無闇むやみをしたと聞く人があるかも知れぬ。別段深い理由でもない。新築の二階から首を出していたら、同級生の一人が冗談に、いくら威張いばっても、そこから飛び降りる事は出来まい。弱虫やーい。と囃たからである。小使に負ぶさって帰って来た時、おやじが大きな眼めをして二階ぐらいから飛び降りて腰を抜かす奴やつがあるかと云いったから、この次は抜かさずに飛んで見せますと答えた。
-                      </div>
+                      <SwipeableList>
+                          <SwipeableListItem
+                              maxSwipe={80} // ここに適切な maxSwipe の値を設定する
+                              leadingActions={leadingActions()}
+                              trailingActions={trailingActions()}
+                          >
+                              親譲りの無鉄砲で小供の時から損ばかりしている。小学校に居る時分学校の二階から飛び降りて一週間ほど腰を抜かした事がある。なぜそんな無闇をしたと聞く人があるかも知れぬ。別段深い理由でもない。新築の二階から首を出していたら、同級生の一人が冗談に、いくら威張っても、そこから飛び降りる事は出来まい。弱虫やーい。と囃たからである。小使に負ぶさって帰って来た時、おやじが大きな眼めをして二階ぐらいから飛び降りて腰を抜かす奴やつがあるかと云いったから、この次は抜かさずに飛んで見せますと答えた。
+                          </SwipeableListItem>
+                      </SwipeableList>
                       {translateError && (
                             <div className={'text-red-500'}>
                                 Translation error
@@ -95,6 +124,7 @@ export const PostOnlyPage: React.FC<Props> = (props: Props) => {
                   </div>
                   <div className={ReactionButtonContainer()}>
                       <FontAwesomeIcon icon={faComment} className={ReactionButton()}></FontAwesomeIcon>
+                      <FontAwesomeIcon icon={faQuoteLeft} className={ReactionButton()}></FontAwesomeIcon>
                       <FontAwesomeIcon icon={faRetweet} className={ReactionButton()}></FontAwesomeIcon>
                       <FontAwesomeIcon icon={!isLiked ? faRegularSquare : faSolidSquare} className={ReactionButton()}></FontAwesomeIcon>
                       <FontAwesomeIcon icon={!isBookmarked ? faRegularBookmark: faSolidBookmark} className={ReactionButton()}></FontAwesomeIcon>
@@ -106,18 +136,18 @@ export const PostOnlyPage: React.FC<Props> = (props: Props) => {
                                 {/*@ts-ignore*/}
                                 <DropdownSection title="Actions" showDivider>
                                     {!isPostMine && (
-                                        <DropdownItem key="new"
+                                        <DropdownItem key="report"
                                                       startContent={<FontAwesomeIcon icon={faFlag} />}
                                         >
                                             Report
                                         </DropdownItem>
                                     )}
-                                    <DropdownItem key="copy"
+                                    <DropdownItem key="share"
                                                   startContent={<FontAwesomeIcon icon={faArrowUpFromBracket} />}
                                     >
                                         Share
                                     </DropdownItem>
-                                    <DropdownItem key="copy"
+                                    <DropdownItem key="translate"
                                                   startContent={<FontAwesomeIcon icon={faLanguage} />}
                                                   onClick={async() => {
                                                       setIsTranslated(true)
@@ -133,29 +163,26 @@ export const PostOnlyPage: React.FC<Props> = (props: Props) => {
                                                                   return acc;
                                                               }, '');
                                                                 setTranslatedText(combinedText)
-                                                                console.log(combinedText)
                                                           }
 
                                                       } else {
                                                           setTranslateError(true)
                                                       }
-                                                      //console.log(await res.json())
-                                                      //setTranslatedText()
                                                   }}
                                     >
                                         Translate
                                     </DropdownItem>
                                 </DropdownSection>
                                 <DropdownSection title="Copy" showDivider={isPostMine}>
-                                    <DropdownItem key="copy" startContent={<FontAwesomeIcon icon={faCode} />}
+                                    <DropdownItem key="json" startContent={<FontAwesomeIcon icon={faCode} />}
                                     >
                                         JSON
                                     </DropdownItem>
-                                    <DropdownItem key="copy" startContent={<FontAwesomeIcon icon={faU} />}
+                                    <DropdownItem key="uri" startContent={<FontAwesomeIcon icon={faU} />}
                                     >
                                         Post URI
                                     </DropdownItem>
-                                    <DropdownItem key="copy" startContent={<FontAwesomeIcon icon={faUser} />}
+                                    <DropdownItem key="did" startContent={<FontAwesomeIcon icon={faUser} />}
                                     >
                                         Author DID
                                     </DropdownItem>
