866bf96a322f4ef80475dbdf42180365b260d912
ば <masaya.kurata@std.code.ac.jp>
2023-09-17 01:23:13 +0900

    fix code

--- apps/web/src/components/pages/ViewSideBar/styles.ts
+++ apps/web/src/components/pages/ViewSideBar/styles.ts
@@ -15,26 +15,18 @@ export const viewSideBar = tv({
     variants: {
         color:{
             light: {
-                background: 'bg-cover bg-[url("https://raw.githubusercontent.com/kawaikute-gomen/ucho-ten-images-repo/main/images/backgroundImages/light/sky_00421.jpg")]',
-                PostModal: 'bg-[#DADADA] bg-opacity-70 text-white',
                 footer: 'bg-[#DADADA]',
                 AuthorHandle: 'text-[#767676]',
             },
             dark: {
-                background: 'bg-cover bg-[url("https://raw.githubusercontent.com/kawaikute-gomen/ucho-ten-images-repo/main/images/backgroundImages/dark/starry-sky-gf5ade6b4f_1920.jpg")]',
-                PostModal: 'bg-[#2C2C2C] bg-opacity-70 text-[#D7D7D7]',
                 footer: 'bg-[#2C2C2C]',
                 AuthorHandle: 'text-[#D7D7D7]',
             },
         },
         isMobile: {
             true: {
-                PostModal: "rounded-none",
-                background: "",
             },
             false: {
-                PostModal: "rounded-[10px] overflow-hidden min-h-[400px]",
-                //background: "relative justify-center items-center",
             },
         },
     }
